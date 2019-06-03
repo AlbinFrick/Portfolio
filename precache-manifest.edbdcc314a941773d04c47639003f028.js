5cd984b0c3def90dc0c3197604b212bb6af32a35
Albin Frick <id16afk@cs.umu.se>
2019-06-03 15:09:08 +0200

    Updates

---
+++ precache-manifest.edbdcc314a941773d04c47639003f028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f86ac71785d88bccbc525e3327047fa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2329097c065d3e4bc86",
+    "url": "/portfolio/static/css/main.31fc5f0d.chunk.css"
+  },
+  {
+    "revision": "1adb0886f739d4995c92",
+    "url": "/portfolio/static/js/2.8d01ba49.chunk.js"
+  },
+  {
+    "revision": "b2329097c065d3e4bc86",
+    "url": "/portfolio/static/js/main.75380dc0.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bc1c18a0f97d921c4226d5b007984877",
+    "url": "/portfolio/static/media/proffe.bc1c18a0.jpeg"
+  }
+]);
