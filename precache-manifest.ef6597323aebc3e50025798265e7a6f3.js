bef9977cd8733be1028bb7f53bfebcbeb0d07f55
Albin Frick <id16afk@cs.umu.se>
2019-06-07 01:36:55 +0200

    Updates

---
+++ precache-manifest.ef6597323aebc3e50025798265e7a6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5991f645607510e12792f9b4ea9e685",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0178c515cad5cb652d0d",
+    "url": "/Portfolio/static/css/main.c2b53cc8.chunk.css"
+  },
+  {
+    "revision": "60103ef457d90e79aa91",
+    "url": "/Portfolio/static/js/2.cfd78722.chunk.js"
+  },
+  {
+    "revision": "0178c515cad5cb652d0d",
+    "url": "/Portfolio/static/js/main.9ec167bc.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "bc1c18a0f97d921c4226d5b007984877",
+    "url": "/Portfolio/static/media/proffe.bc1c18a0.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.1488f31afe1ef0df4ff1e46d0a9cd099.js"
+  "/Portfolio/precache-manifest.ef6597323aebc3e50025798265e7a6f3.js"
 );
 
 self.addEventListener('message', (event) => {
