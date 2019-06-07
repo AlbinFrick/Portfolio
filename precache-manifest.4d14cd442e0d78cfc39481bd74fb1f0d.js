1ba9cce704bb64cc2051e7074a06f449bf3ecca0
Albin Frick <id16afk@cs.umu.se>
2019-06-07 16:50:03 +0200

    Updates

--- precache-manifest.4d14cd442e0d78cfc39481bd74fb1f0d.js
+++ precache-manifest.4d14cd442e0d78cfc39481bd74fb1f0d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1f073775bb939d3433becb9b46a3ffe4",
+    "revision": "eb1be9b833c856e41d9ebfc8223954fd",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "34ee845ce4e3a26e8d70",
-    "url": "/Portfolio/static/css/main.6b223e6f.chunk.css"
+    "revision": "2d4302d815c09ddd5f2d",
+    "url": "/Portfolio/static/css/main.fc6047b1.chunk.css"
   },
   {
     "revision": "974db6a4969ef5dc0444",
     "url": "/Portfolio/static/js/2.d9239d10.chunk.js"
   },
   {
-    "revision": "34ee845ce4e3a26e8d70",
-    "url": "/Portfolio/static/js/main.ebedd8fb.chunk.js"
+    "revision": "2d4302d815c09ddd5f2d",
+    "url": "/Portfolio/static/js/main.31e7fe51.chunk.js"
   },
   {
     "revision": "6b64d540f1308ee928ad",
